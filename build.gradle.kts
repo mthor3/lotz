e7620808a15556143f6fa6281eb30e2953675783
mthor3 <127059026+mthor3@users.noreply.github.com>
2026-07-17 21:57:50 -0700

    feat: update build configuration with Kotlin JVM plugin and source sets

--- build.gradle.kts
+++ build.gradle.kts
@@ -1,8 +1,19 @@
 plugins {
+    kotlin("jvm") version "2.2.0"
     alias(libs.plugins.kotlinMultiplatform) apply false
     alias(libs.plugins.kotlinSerialization) apply false
     alias(libs.plugins.composeMultiplatform) apply false
     alias(libs.plugins.composeCompiler) apply false
     alias(libs.plugins.androidApplication) apply false
     alias(libs.plugins.androidLibrary) apply false
+}
+
+
+dependencies {
+    implementation(kotlin("stdlib"))
+}
+
+kotlin {
+    sourceSets["main"].kotlin.srcDir("src/main/java")
+    sourceSets["test"].kotlin.srcDir("src/test/java")
 }
